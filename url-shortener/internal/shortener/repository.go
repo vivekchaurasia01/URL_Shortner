441a1780e29f72cf57ce710677bde047becfaa82
Vivek chaurasia <vivekkumarr221106@gmail.com>
2026-07-16 23:53:48 +0530

    to make our retry logic safe ,error type has been distinguished

--- url-shortener/internal/shortener/repository.go
+++ url-shortener/internal/shortener/repository.go
@@ -2,7 +2,9 @@ package shortener
 
 import (
 	"context"
+	"errors"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -21,6 +23,20 @@ func (r *URLPostregesRepository) Save (mapping URLMapping) error {
 		mapping.LongURL,
 		mapping.ShortURL,
 	)
+	if err != nil {  // err type -> same short url exist || or db down etc.....
+		// we need to identify type of error so we can identify if we  should retry or not...
+		// check if it's specifically a collision...
+		var pgErr *pgconn.PgError
+		if errors.As(err,&pgErr) && pgErr.Code == "23505" {  //23505 collison err..
+			return ErrDuplicateShortURL
+		}
+		return err  // other err than collision...
+	}
+	return nil
 
+	
+}
+
+func (r *URLPostregesRepository) FindByShortURL (shortURL string) (*URLMapping, error) {
 	
 }
