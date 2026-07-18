d749474f10736eaf34da91f57e56fec4dc9189b8
Vivek chaurasia <vivekkumarr221106@gmail.com>
2026-07-18 20:51:05 +0530

    assembling shortner service in main

--- url-shortener/cmd/shortener/main.go
+++ url-shortener/cmd/shortener/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/vivekchaurasia01/url-shortener/internal/shortener"
+)
+
+
+func main () {
+	dbStr := os.Getenv("DATABASE_URL")  // we didnt write our string directly because we dont want it to leak on git after push..
+
+	if dbStr == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+
+	pool,err := pgxpool.New(context.Background(),dbStr)
+	if err != nil {
+		log.Fatalf("cant connect to database : %v",err)
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(context.Background()); err != nil {
+		log.Fatalf("cant ping database : %v",err)
+	}
+
+	repo := shortener.NewURLRepository(pool)
+	handler := shortener.NewHandler(repo)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/shorten",handler.ShortenURL)
+
+	log.Println("shortener service running on :8080")
+	log.Fatal(http.ListenAndServe(":8080",mux))
+
+
+}
