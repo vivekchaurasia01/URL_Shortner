ca178169862fbb20f75ea124857f5fa151a8c997
Vivek chaurasia <vivekkumarr221106@gmail.com>
2026-07-20 18:04:23 +0530

    interface instead of struct with constrctor for better testing and swapping

--- url-shortener/internal/redirector/models.go
+++ url-shortener/internal/redirector/models.go
@@ -1,2 +1,8 @@
 package redirector
 
+
+type URLMapping struct {
+    LongURL  string
+    ShortURL string
+}
+
--- url-shortener/internal/redirector/service.go
+++ url-shortener/internal/redirector/service.go
@@ -1,1 +1,34 @@
 package redirector
+
+import "errors"
+
+var ErrNotFound = errors.New("short URL not found")  //New returns an error that formats as the given text. 
+
+type URLRepository interface {
+    FindByShortURL(shortURL string) (*URLMapping, error)
+}
+
+type CacheRepository interface {
+    Get(shortURL string) (string, error)
+    Set(shortURL string, longURL string) error
+}
+
+func ResolveLongURL(shortURL string, cache CacheRepository, repo URLRepository) (string, error) {
+    // check Redis first....
+    longURL, err := cache.Get(shortURL)
+    if err == nil {
+        // short url found in redis, DB never touched....
+        return longURL, nil
+    }
+
+    // cache miss — request forward to Postgres.......
+    mapping, err := repo.FindByShortURL(shortURL)
+    if err != nil {
+        return "", ErrNotFound
+    }
+
+    // fill cache so next request for this code is a hit...
+    cache.Set(shortURL, mapping.LongURL)
+
+    return mapping.LongURL, nil
+}
